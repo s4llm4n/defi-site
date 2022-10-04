3f71e820780908196fde8cfb52bb40378ee89352
s4llm4n <s4llm4n@gmail.com>
2022-10-04 11:01:06 +0700

    commit footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,25 +7,39 @@ const Footer = () => {
     <div className='footer'>
         <div className='container'>
             <div className='col col-1'>
-                <h1>Sal<Span className='primary'>Man</Span></h1>
+                <h1>Sal<span className='primary'>Man</span></h1>
             </div>
             <div className='col'>
                 <h5>Support</h5>
-                <span className='col'>
+                <span className='bar'></span>
                     <a href='/'>Contact Us</a>
                     <a href='/'>Chat</a>
                     <a href='/'>Help Center</a>
                     <a href='/'>FAQ</a>
-                </span>
             </div>
             <div className='col'>
                 <h5>Developers</h5>
-                <span className='col'>
-                    <a href='/'>Contact Us</a>
-                    <a href='/'>Chat</a>
-                    <a href='/'>Help Center</a>
-                    <a href='/'>FAQ</a>
-                </span>
+                <span className='bar'></span>
+                    <a href='/'>Cloud</a>
+                    <a href='/'>Commerce</a>
+                    <a href='/'>Pro</a>
+                    <a href='/'>API</a>
+            </div>
+            <div className='col'>
+                <h5>Company</h5>
+                <span className='bar'></span>
+                    <a href='/'>About</a>
+                    <a href='/'>Information</a>
+                    <a href='/'>Legal</a>
+                    <a href='/'>Privacy</a>
+            </div>
+            <div className='col'>
+                <h5>Social</h5>
+                <span className='bar'></span>
+                    <a href='/'><FaFacebook className='icon' /></a>
+                    <a href='/'><FaTwitter className='icon' /></a>
+                    <a href='/'><FaLinkedin className='icon' /></a>
+                    <a href='/'><FaGithub className='icon' /></a>
             </div>
         </div>
     </div>
